3045c3dd7838bc62e6fee1d8861fd8797aa378f7
dordory@gmail.com <arabim@MacBookPro.local>
2016-09-21 17:42:30 +0900

    카렌다템플릿 수정

--- tks/apps/calendar/models.py
+++ tks/apps/calendar/models.py
@@ -7,4 +7,5 @@ class task(models.Model):
 
     task_date = models.DateField()
     title = models.CharField(max_length=512, null=False)
+    locate = models.CharField(max_length=256, null=True)
     content = models.TextField(max_length=2014, null=True)
