7b1a624592c8a4ffab853159c4e6d630c31b3307
dordory@gmail.com <arabim@MacBookPro.local>
2016-09-28 01:11:43 +0900

    url수정

--- tks/apps/news/urls.py
+++ tks/apps/news/urls.py
@@ -6,9 +6,10 @@ from . import views as view
 
 urlpatterns = [
     url("^tokan/$", view.TokanNewsListView.as_view(), name="tokannews_list"),
-    url("^tokan/(?P<pk>\d+)/", view.TokanNewsView.as_view(), name="tokannews_detail"),
-    url("^tax/$", view.TokanNewsListView.as_view(), name="tokannews_list"),
-    url("^tax/(?P<pk>\d+)/", view.TokanNewsView.as_view(), name="tokannews_detail"),
-    url("^week/$", view.TokanNewsListView.as_view(), name="tokannews_list"),
-    url("^week/(?P<pk>\d+)/", view.TokanNewsView.as_view(), name="tokannews_detail"),
+    # url("^tokan/(?P<pk>\d+)/", view.TokanNewsView.as_view(), name="tokannews_detail"),
+    url("^tokan/(?P<pk>\d+)/", view.DisplayPDFView.as_view(), name="tokannews_detail"),
+    url("^tax/$", view.TaxNewsListView.as_view(), name="tokannews_list"),
+    url("^tax/(?P<pk>\d+)/", view.TaxNewsView.as_view(), name="tokannews_detail"),
+    url("^week/$", view.WeekNewsListView.as_view(), name="tokannews_list"),
+    url("^week/(?P<pk>\d+)/", view.WeekNewsView.as_view(), name="tokannews_detail"),
 ]
