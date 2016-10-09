57ef5ac138694d37d858c46805f85e1333a55da7
SeongUk Yun <dordory@gmail.com>
2016-10-10 02:34:31 +0900

    멤버와 인증정보 자동로드 코맨드, 로그인 메뉴제어

--- tks/apps/member/management/commands/member_load_csv.py
+++ tks/apps/member/management/commands/member_load_csv.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 from django.db import transaction
 from backports import csv
 import io
@@ -19,19 +20,30 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        print os.path.basename(options['filename'])
 
+        def create_mail(str):
+            seed = str.replace('-', '')
+            return '{0}@tokansho.org'.format(seed)
+
+        print os.path.basename(options['filename'])
         os.chdir(os.path.dirname(options['filename']))
         filename = os.path.basename(options['filename'])
 
         reader = csv.reader(io.open(filename, encoding="utf-8"),delimiter=',')
         reader.next()
         for i, row in enumerate(reader):
-            print '{0}-{1}'.format(row[2], row[3])
-            print '{0}-{1}-{2}-{3}'.format(row[1], row[4], row[5], row[6])
-            print '{0}-{1}-{2}-{3}'.format(row[7], row[8], row[9], row[10])
+            if not row[11]:
+                continue
+            user = User.objects.create_user(row[11].replace('-', ''),
+                                            create_mail(row[11]),
+                                            row[11].replace('-', ''))
 
-            Member.objects.update_or_create(
+            # print '{0}-{1}'.format(row[2], row[3])
+            # print '{0}-{1}-{2}-{3}'.format(row[1], row[4], row[5], row[6])
+            # print '{0}-{1}-{2}-{3}'.format(row[7], row[8], row[9], row[10])
+
+            member, c = Member.objects.update_or_create(
+                auth_user = user,
                 name = row[2],
                 popular_name = row[3],
                 defaults = {
@@ -45,3 +57,11 @@ class Command(BaseCommand):
                     'zipcode': row[9].strip(),
                     'address': row[10],
                     })
+
+            _first_name =  member.first_name()
+            _last_name = member.last_name()
+            if _first_name:
+                user.first_name = member.first_name()
+            if _last_name:
+                user.last_name = member.last_name()
+            user.save()
--- tks/apps/member/models.py
+++ tks/apps/member/models.py
@@ -2,14 +2,29 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class member(models.Model):
+
+    header = ['公開範囲',
+              '職位',
+              '姓名',
+              '通称名',
+              '役職',
+              '会社名',
+              '業種',
+              '会社TEL',
+              '会社FAX',
+              '郵便番号',
+              '会社住所',
+              '携帯番号']
 
     rank_choices = (('00', '名誉会長'), ('01', '会長'), ('02', '部会長'),
                     ('03', '常任理事'), ('04', '監事'))
     accessibility_choices = ((1, '公開'), (2, '会員のみ'), (3, '非公開'))
 
+    auth_user = models.ForeignKey('auth.User', null=True, blank=True, related_name='member')
     accessibility = models.IntegerField(choices=accessibility_choices)
     rank = models.CharField(max_length=2, choices=rank_choices)
     name = models.CharField(max_length=128, null=False)
@@ -27,3 +42,12 @@ class member(models.Model):
         'created time', auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(
         'updated time', auto_now=True, blank=True, null=True)
+
+    def parsedName(self):
+        return self.name.split(' ')
+
+    def first_name(self):
+        return self.name.split(' ')[1]
+
+    def last_name(self):
+        return self.name.split(' ')[0]
