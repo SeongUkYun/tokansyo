1fcfe95886310100ac3ce66c418496f6f44413fd
SeongUk Yun <dordory@gmail.com>
2016-10-25 21:22:25 +0900

    상담폼의 text와 textarea 길이를 맞춤

--- tks/apps/inquiry/forms.py
+++ tks/apps/inquiry/forms.py
@@ -8,6 +8,7 @@ from models import inquiry as Inquiry
 class InquiryForm(forms.ModelForm):
     email = forms.CharField(widget=forms.TextInput(attrs={'size': 42}))
     title = forms.CharField(widget=forms.TextInput(attrs={'size': 42}))
+    content = forms.Field(widget=forms.Textarea(attrs={'style': 'width: 42;'}))
 
     class Meta:
         model = Inquiry
