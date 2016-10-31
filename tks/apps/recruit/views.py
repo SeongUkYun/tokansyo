593ec8a26ab2d0b01bae350ed51f8af7ef358e4d
SeongUk Yun <dordory@gmail.com>
2016-10-31 14:27:28 +0900

    리크루트 게시 신청이 있으면 메일 송신

--- tks/apps/recruit/views.py
+++ tks/apps/recruit/views.py
@@ -52,4 +52,16 @@ class RecruitApplyCompleteView(TemplateView):
         form = RecruitForm(request.POST, request.FILES)
         form.save()
 
+        content = '求人情報掲載依頼がありました。\n'
+        content += '登録IDは{0}です。\n'.format(recruit.id)
+        content += '管理画面から掲載可否をチェックしてください。'
+        message = EmailMessage(
+            '求人掲載依頼がありました。',
+            content,
+            'recruit@tokansho.org',
+            ["tks@tokansho.org"],
+            ["dordory@gmail.com"],
+            headers={'Reply-To': 'tks@tokansho.org'})
+        message.send()
+
         return render(request, self.template_name)
