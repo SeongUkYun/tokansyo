af97e9038dbb650e5d4518243b5d7c7409332e0f
SeongUk Yun <dordory@gmail.com>
2017-09-07 01:16:20 +0900

    화면크기에 맞춰 이미지 폭 자동조절

--- static/js/sliderEffects.js
+++ static/js/sliderEffects.js
@@ -13,11 +13,12 @@ function codeAddress() {
 
 function fullScreen () {
     var dif = (window.innerWidth / parseInt(document.getElementById("upper_slider").style.width) );
-    document.getElementById("upper_slider").style.width = window.innerWidth + "px";
-    document.getElementById("upper_slider").style.height = (dif  * parseInt(document.getElementById("upper_slider").style.height) ) + "px";
+    document.getElementById("upper_slider").style.width = "auto";
+    document.getElementById("upper_slider").style.height = "200px";
     
     var firstIMG = document.getElementById("upper_slider").getElementsByTagName('img')[0];
-    firstIMG.style.width =  window.innerWidth + "px";
+    firstIMG.width =  "100%";
+    firstIMG.height = "100%";
     
     window.addEventListener("resize", fullScreen);
 
@@ -25,15 +26,18 @@ function fullScreen () {
 }
 
 function KSTransitionTimer(imgNext) {
-        clearInterval(timer);
+        // clearInterval(timer);
+
+        upper_slider.innerHTML = "";
+        upper_slider.style.width = "auto";
+        upper_slider.style.height = "200px";
 
         var imagePart = document.createElement('img');
-        imagePart.src = imgNext;
-        upper_slider.innerHTML = "";
-
         upper_slider.appendChild(imagePart);
-        imagePart.width = upper_slider.width;
-        imagePart.height = upper_slider.height;
+        imagePart.setAttribute('width', '100%');
+        imagePart.setAttribute('height', '100%');
+
+        imagePart.src = imgNext;
  }
 
 //Effects 
@@ -42,251 +46,136 @@ function KSDissolve ( image, imgNext, nextLink) {
     
     codeAddress();
     upper_slider.innerHTML = "";
+    upper_slider.style.width = "auto";
+    upper_slider.style.height = "200px";
+
+    var iDiv = document.createElement('div');
+    iDiv.className = 'dissolve';
+    iDiv.style.width = "auto";
+    iDiv.style.height = "200px";
+    upper_slider.appendChild(iDiv);
+
+    var imagePart = document.createElement('img');
+    imagePart.src = image;
+    imagePart.setAttribute('width', '100%');
+    imagePart.setAttribute('height', '100%');
+    iDiv.appendChild(imagePart);
+
     upper_slider.style.background = "url(" + imgNext + ")";
     upper_slider.style.backgroundSize = "cover";
     upper_slider.style.backgroundOrigin = "center";
     upper_slider.setAttribute('data-url', nextLink);
 
-    var width = parseInt(upper_slider.style.width);
-    var height = parseInt(upper_slider.style.height);
-
-    var iDiv = document.createElement('div');
-    iDiv.className = 'dissolve';
-    iDiv.style.width = width + "px";
-
-    var imagePart = document.createElement('img');
-    imagePart.src = image;
-    imagePart.width = width;
-
-    iDiv.appendChild(imagePart);
-    upper_slider.appendChild(iDiv);
-    
-    timer = setInterval(function () {KSTransitionTimer(imgNext)}, 1000);
-
+    // timer = setInterval(function () {KSTransitionTimer(imgNext)}, 100);
+    KSTransitionTimer(imgNext);
 } 
 
 function KSSlideUp ( image, imgNext, nextLink) {
     
     codeAddress();
     upper_slider.innerHTML = "";
+    upper_slider.style.width = "auto";
+    upper_slider.style.height = "200px";
+    
+    var iDiv = document.createElement('div');
+    iDiv.className = 'slideUp';
+    iDiv.style.width = "auto";
+    iDiv.style.height = "200px";
+    upper_slider.appendChild(iDiv);
+
+    var imagePart = document.createElement('img');
+    imagePart.src = image;
+    imagePart.width = "100%";
+    imagePart.height = "100%";
+    iDiv.appendChild(imagePart);
+    
     upper_slider.style.background = "url(" + imgNext + ")";
     upper_slider.style.backgroundSize = "cover";
     upper_slider.setAttribute('data-url', nextLink);
 
-    var width = parseInt(upper_slider.style.width);
-    var height = parseInt(upper_slider.style.height);
-
-
-    var iDiv = document.createElement('div');
-    iDiv.className = 'slideUp';
-    iDiv.style.width = width + "px";
-
-    var imagePart = document.createElement('img');
-    imagePart.src = image;
-    imagePart.width = width;
-    imagePart.height = height;
-
-
-    iDiv.appendChild(imagePart);
-    upper_slider.appendChild(iDiv);
-    
-    timer = setInterval(function () {KSTransitionTimer(imgNext)}, 1000);
-
+    // timer = setInterval(function () {KSTransitionTimer(imgNext)}, 100);
+    KSTransitionTimer(imgNext);
 } 
 
 function KSSlideDown ( image, imgNext, nextLink) {
     
     codeAddress();
     upper_slider.innerHTML = "";
+    upper_slider.style.width = "auto";
+    upper_slider.style.height = "200px";
+    
+    var iDiv = document.createElement('div');
+    iDiv.className = 'slideDown';
+    iDiv.style.width = "auto";
+    iDiv.style.height = "200px";
+    upper_slider.appendChild(iDiv);
+
+    var imagePart = document.createElement('img');
+    imagePart.src = image;
+    imagePart.setAttribute('width', '100%');
+    imagePart.setAttribute('height', '100%');
+    iDiv.appendChild(imagePart);
+
     upper_slider.style.background = "url(" + imgNext + ")";
     upper_slider.style.backgroundSize = "cover";
     upper_slider.setAttribute('data-url', nextLink);
 
-    var width = parseInt(upper_slider.style.width);
-    var height = parseInt(upper_slider.style.height);
-
-
-    var iDiv = document.createElement('div');
-    iDiv.className = 'slideDown';
-    iDiv.style.width = width + "px";
-
-    var imagePart = document.createElement('img');
-    imagePart.src = image;
-    imagePart.width = width;
-    imagePart.height = height;
-
-    iDiv.appendChild(imagePart);
-    upper_slider.appendChild(iDiv);
-
-    timer = setInterval(function () {KSTransitionTimer(imgNext)}, 1000);
-
+    // timer = setInterval(function () {KSTransitionTimer(imgNext)}, 100);
+    KSTransitionTimer(imgNext);
 } 
 
 function KSSlideLeft ( image, imgNext, nextLink) {
     
     codeAddress();
     upper_slider.innerHTML = "";
+    upper_slider.style.width = "auto";
+    upper_slider.style.height = "200px";
+
+    var iDiv = document.createElement('div');
+    iDiv.className = 'slideLeft';
+    iDiv.style.width = "auto";
+    iDiv.style.height = "200px";
+    upper_slider.appendChild(iDiv);
+
+    var imagePart = document.createElement('img');
+    imagePart.src = image;
+    imagePart.width = "100%";
+    imagePart.height = "100%";
+    iDiv.appendChild(imagePart);
+    
     upper_slider.style.background = "url(" + imgNext + ")";
     upper_slider.style.backgroundSize = "cover";
     upper_slider.setAttribute('data-url', nextLink);
 
-    var width = parseInt(upper_slider.style.width);
-    var height = parseInt(upper_slider.style.height);
-
-
-    var iDiv = document.createElement('div');
-    iDiv.className = 'slideLeft';
-    iDiv.style.width = width + "px";
-
-    var imagePart = document.createElement('img');
-    imagePart.src = image;
-    imagePart.width = width;
-    imagePart.height = height;
-
-
-    iDiv.appendChild(imagePart);
-    upper_slider.appendChild(iDiv);
-    
-    timer = setInterval(function () {KSTransitionTimer(imgNext)}, 1000);
-
+    // timer = setInterval(function () {KSTransitionTimer(imgNext)}, 100);
+    KSTransitionTimer(imgNext);
 } 
 
 function KSSlideRight ( image, imgNext, nextLink) {
     
     codeAddress();
     upper_slider.innerHTML = "";
-    upper_slider.style.background = "url(" + imgNext + ")";
-    upper_slider.style.backgroundSize = "cover";
-    upper_slider.setAttribute('data-url', nextLink);
-
-    var width = parseInt(upper_slider.style.width);
-    var height = parseInt(upper_slider.style.height);
-
+    upper_slider.style.width = "auto";
+    upper_slider.style.height = "200px";
 
     var iDiv = document.createElement('div');
     iDiv.className = 'slideRight';
-    iDiv.style.width = width + "px";
+    iDiv.style.width = "auto";
+    iDiv.style.height = "200px";
+    upper_slider.appendChild(iDiv);
 
     var imagePart = document.createElement('img');
     imagePart.src = image;
-    imagePart.width = width;
-    imagePart.height = height;
-
-
+    imagePart.width = "100px";
+    imagePart.height = "100px";
     iDiv.appendChild(imagePart);
-    upper_slider.appendChild(iDiv);
-    
-    timer = setInterval(function () {KSTransitionTimer(imgNext)}, 1000);
 
-} 
-
-function KSMosaic ( image, imgNext, nextLink) {
-
-    codeAddress();
-    upper_slider.innerHTML = "";
     upper_slider.style.background = "url(" + imgNext + ")";
     upper_slider.style.backgroundSize = "cover";
     upper_slider.setAttribute('data-url', nextLink);
 
-    var width = parseInt(upper_slider.style.width);
-    var height = parseInt(upper_slider.style.height);
-
-
-    for (var i = 0; i < Math.floor(height/100); i ++) {
-        for (var j = 0; j < Math.floor(width/100); j++) {
-            var iDiv = document.createElement('div');
-            iDiv.className = 'mosaicBlock';
-
-            var imagePart = document.createElement('img');
-            imagePart.src = image;
-            imagePart.width = width;
-            imagePart.height = height;
-            imagePart.style.transform = "translate("+(-j*100)+"px,"+(-i*100)+"px)";
-            imagePart.style.webkitTransform  = "translate("+(-j*100)+"px,"+(-i*100)+"px)";
-            imagePart.style.MozTransform = "translate("+(-j*100)+"px,"+(-i*100)+"px)";
-            
-            iDiv.appendChild(imagePart);
-
-            upper_slider.appendChild(iDiv);
-        }
-    }
-
-    timer = setInterval(function () {KSTransitionTimer(imgNext)}, 1000);
-
-}
-
-function KSWindows ( image, imgNext, nextLink) {
-    
-    codeAddress();
-    upper_slider.innerHTML = "";
-    upper_slider.style.background = "url(" + imgNext + ")";
-    upper_slider.style.backgroundSize = "cover";
-    upper_slider.setAttribute('data-url', nextLink);
-
-    var width = parseInt(upper_slider.style.width);
-    var height = parseInt(upper_slider.style.height);
-
-
-    for (var i = 0; i < width/300; i ++) {
-        //for (var j = 0; j < width/100; j++) {
-            var iDiv = document.createElement('div');
-            iDiv.className = 'windowBlock';
-
-            var imagePart = document.createElement('img');
-            imagePart.src = image;
-            imagePart.width = width;
-            imagePart.height = height;
-            imagePart.style.transform  = "translate("+(-i* (300) )+"px,0px)";
-            imagePart.style.webkitTransform  = "translate("+(-i* (300) )+"px,0px)";
-            imagePart.style.MozTransform  = "translate("+(-i* (300) )+"px,0px)";
-
-
-            iDiv.appendChild(imagePart);
-
-            upper_slider.appendChild(iDiv);
-        //}
-    }
-
-
-    timer = setInterval(function () {KSTransitionTimer(imgNext)}, 1000);
-
-
-}
-
-function KSDoors ( image, imgNext, nextLink) {
-    
-    codeAddress();
-    upper_slider.innerHTML = "";
-    upper_slider.style.background = "url(" + imgNext + ")";
-    upper_slider.style.backgroundSize = "cover";
-    upper_slider.setAttribute('data-url', nextLink);
-
-    var width = parseInt(upper_slider.style.width);
-    var height = parseInt(upper_slider.style.height);
-
-
-    for (var i = 0; i < 2; i ++) {
-        var iDiv = document.createElement('div');
-        iDiv.className = i > 0 ? 'imagePush' : 'imagePush2';
-        iDiv.style.width = width/2 + "px";
-
-        var imagePart = document.createElement('img');
-        imagePart.src = image;
-        imagePart.width = width;
-        imagePart.height = height;
-
-        imagePart.style.transform  = "translate("+(-i* (width/2 ) )+"px,0px)";
-        imagePart.style.webkitTransform  = "translate("+(-i* (width/2 ) )+"px,0px)";
-        imagePart.style.MozTransform  = "translate("+(-i* (width/2 ) )+"px,0px)";
-
-        iDiv.appendChild(imagePart);
-        
-        upper_slider.appendChild(iDiv);
-    }
-
-
-    timer = setInterval(function () {KSTransitionTimer(imgNext)}, 1000);
-
+    // timer = setInterval(function () {KSTransitionTimer(imgNext)}, 100);
+    KSTransitionTimer(imgNext);
 } 
 
 window.onload = codeAddress();
