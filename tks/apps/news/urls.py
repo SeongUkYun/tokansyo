92e784d608287a70476b4291c3b7ce2caaaf192a
SeongUk Yun <dordory@gmail.com>
2017-10-17 01:29:41 +0900

    베너표시와 일부버그 수정

--- tks/apps/news/urls.py
+++ tks/apps/news/urls.py
@@ -7,8 +7,8 @@ from . import views as view
 urlpatterns = [
     url("^tokan/$", view.TokanNewsListView.as_view(), name="tokannews_list"),
     url("^tokan/(?P<pk>\d+)/", view.TokanNewsView.as_view(), name="tokannews_detail"),
-    url("^tax/$", view.TaxNewsListView.as_view(), name="tokannews_list"),
-    url("^tax/(?P<pk>\d+)/", view.TaxNewsView.as_view(), name="tokannews_detail"),
-    url("^week/$", view.WeekNewsListView.as_view(), name="tokannews_list"),
-    url("^week/(?P<pk>\d+)/", view.WeekNewsView.as_view(), name="tokannews_detail"),
+    url("^tax/$", view.TaxNewsListView.as_view(), name="taxnews_list"),
+    url("^tax/(?P<pk>\d+)/", view.TaxNewsView.as_view(), name="taxnews_detail"),
+    url("^week/$", view.WeekNewsListView.as_view(), name="weeknews_list"),
+    url("^week/(?P<pk>\d+)/", view.WeekNewsView.as_view(), name="weeknews_detail"),
 ]
